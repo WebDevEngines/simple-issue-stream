67b6b0f1df8e53ab1fbbf9327ec296f16512aa02
nickcrafford <nickcrafford@gmail.com>
2016-09-04 20:56:55 -0700

    XML instead of JSON for Google Sheets Support

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ from flask import Flask
 from flask import request
 
 import re
-import json
-import os
 
 
 app = Flask(__name__)
@@ -24,47 +22,32 @@ def get_issues():
 
     if username and password and repository:
         def stream_issues(username, password, repo_name):
-            yield "["
+            yield "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rows>"
             g = Github(username, password)
             states = ["open", "closed"]
-            issues = []
-            first_issue = True
             for repo in g.get_user().get_repos():
                 if repo.name == repo_name:
                     for state in states:
                         for issue in repo.get_issues(state=state):
-                            if first_issue:
-                                first_issue = False
-                            else:
-                                yield ","
-
-                            assignee = issue.assignee
-                            obj = {
-                                "id": issue.id,
-                                "state": state,
-                                "number" : issue.number,
-                                "title": issue.title,
-                                "assignee": assignee.name if assignee else None,
-                                "labels": ",".join([x.name for x in issue.labels]),
-                                "created_at": issue.created_at.strftime("%m/%d/%Y"),
-                                "closed_at": issue.closed_at.strftime("%m/%d/%Y") if issue.closed_at else None,
-                                "milestone_title": None,
-                                "milestone_number" : None
-                            }
-
+                            number = issue.number
+                            title = issue.title
+                            assignee = issue.assignee.name if issue.assignee else None
+                            labels = ",".join([x.name for x in issue.labels])
+                            created_at = issue.created_at.strftime("%m/%d/%Y")
+                            closed_at = issue.closed_at.strftime("%m/%d/%Y") if issue.closed_at else None
+                            milestone_title = None
+                            milestone_number = None
                             milestone = issue.milestone
                             if milestone:
-                                obj["milestone_title"] = milestone.title
+                                milestone_title = milestone.title
                                 match = re.search("(\d+) - .*", milestone.title)
                                 if match:
-                                    obj["milestone_number"] = int(match.group(1))
-                            
-                            yield json.dumps(obj)
+                                    milestone_number = int(match.group(1))                            
+                            yield "<row><number>%s</number><state>%s</state><title>%s</title><assignee>%s</assignee><labels>%s</labels><created_at>%s</created_at><closed_at>%s</closed_at><milestone_title>%s</milestone_title><milestone_number>%s</milestone_number></row>" % (number, state, title, assignee, labels, created_at, closed_at, milestone_title, milestone_number)
                     break
-            yield "]"
-
+            yield "</rows>"
         return Response(
-            stream_issues(username, password, repository), mimetype="application/json"
+            stream_issues(username, password, repository), mimetype="text/xml"
         )
     else:
         return Response("""
